300314cf885e786f78124f0e2e3f52c3d1962e3f
Christopher Meehan <chrismeehan91@gmail.com>
2022-12-27 14:22:59 -0600

    first commit

---
+++ Spring/HoppersReceipt/src/main/java/com/chrismeehan/hoppersreceipt/controllers/MainController.java
@@ -0,0 +1,27 @@
+package com.chrismeehan.hoppersreceipt.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        
+        String name = "Horace Gopper";
+        String itemName = "Copper wire";
+        double price = 7.25;
+        String description = "Metal strips, also an illustration of nanoseconds.";
+        String vendor = "Little Things Corner Store";
+        
+        model.addAttribute("name", name);
+        model.addAttribute("itemName", itemName);
+        model.addAttribute("price", price);
+        model.addAttribute("description", description);
+        model.addAttribute("vendor", vendor);
+    
+        return "index.jsp";
+    }
+}
